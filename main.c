28bf2d81f590894c08ece85c47eb0413b5a65c96
윤기 <129282769+Yun-Gi@users.noreply.github.com>
2026-09-25 15:11:52 +0900

    d

--- main.c
+++ main.c
@@ -70,80 +70,44 @@ void update_sonsors(void){
     }
 }
 
-bool error_check(void){
-    if(flags & (ERROR_MASK)){
+void handle_errors(void){
+    if(bit_check(ERROR_MASK)){
         bit_off(FLAG_STOP_REQUEST | FLAG_START_REQUEST);
+        current_state = ST_HALTED;
     }
 }
 
 int main(void){
-    /*while(1){
-        printf("0. CARRIAGE_AT_RIGHT\n");
-        printf("1. CARRIAGE_AT_LEFT\n");
-        printf("2. START_REQUEST\n");
-        printf("3. STOP_REQUEST\n");
-        printf("4. COVER_OPEN\n");
-        printf("5. INK_LOW\n");
-        printf("6. MEDIA_EMPTY\n");
-        printf("7. EXIT\n");
-
-        int input;
-        scanf("%d", &input);
-
-        if(input < 0 || input > 7){
-            printf("False Input\n");
-            continue;
-        }
-        else if(input == 7){
-            return 0;
-        }
-
-        if(bit_check(1u << input)){
-            bit_off(1u << input);
-        }
-        else{
-            bit_on(1u << input);
-        }
-        print_flags();
-        if(bit_check(ERROR_MASK)){
-            printf("ERROR\n");
-        }
-        else{
-            printf("OK\n");
-        }
-    }*/
-
     const char *state_names[] = {"ST_IDLE", "ST_CARRIAGE_SCAN", "ST_MEDIA_FEED", "ST_HALTED", "ST_CARRIAGE_RETURN"};
     int job_size = 3;
     int tick = 0;
 
     while(1){
         update_sonsors();
-        error_check();
+        handle_errors();
         printf("[tick %3d] %-18s pos=%2d feed=%d lines=%d/%d  flags=", tick++, state_names[current_state], carriage_pos, feed_steps, printed_lines, total_lines);
         print_flags();
     
         switch (current_state)
         {
         case ST_IDLE:
-            if(total_lines <= printed_lines && total_lines != 0){
-                return 0;
-            }
-            else if(carriage_pos == 0){
+            if((flags & START_READY_MASK) == START_READY_MASK){
                 total_lines = job_size;
                 printed_lines = 0;
                 feed_steps = 0;
+                bit_off(FLAG_START_REQUEST);
                 current_state = ST_MEDIA_FEED;
             }
-            else{
+            else if((flags & START_READY_MASK) == FLAG_START_REQUEST){
                 total_lines = job_size;
                 printed_lines = 0;
+                bit_off(FLAG_START_REQUEST);
                 current_state = ST_CARRIAGE_RETURN;
             }
             break;
 
         case ST_CARRIAGE_SCAN:
-            if(carriage_pos == CARRIAGE_POS_MAX){
+            if(bit_check(FLAG_CARRIAGE_AT_RIGHT)){
                 printed_lines += 1;
                 current_state = ST_CARRIAGE_RETURN;
             }
@@ -153,15 +117,17 @@ int main(void){
             break;
 
         case ST_HALTED:
-            current_state = ST_IDLE;
+            if(!bit_check(ERROR_MASK)){
+                current_state = ST_IDLE;
+            }
             break;
 
         case ST_CARRIAGE_RETURN:
-            if(carriage_pos == 0 && total_lines > printed_lines){
+            if(bit_check(FLAG_CARRIAGE_AT_LEFT) && total_lines > printed_lines){
                 feed_steps = 0;
                 current_state = ST_MEDIA_FEED;
             }
-            else if(carriage_pos == 0 && total_lines <= printed_lines){
+            else if(bit_check(FLAG_CARRIAGE_AT_LEFT) && total_lines <= printed_lines){
                 printf("Done\n");
                 current_state = ST_IDLE;
             }
@@ -183,8 +149,31 @@ int main(void){
             break;
         }
         
-        getchar();
+        int c = getchar();
+        if (c != '\n') {
+            while(getchar() != '\n'){ }
+            switch(c)
+            {
+            case 's':
+                bit_on(FLAG_START_REQUEST);
+                break;
+            case 'c':
+                if(bit_check(FLAG_COVER_OPEN)){
+                    bit_off(FLAG_COVER_OPEN);
+                }
+                else{
+                    bit_on(FLAG_COVER_OPEN);
+                }
+                break;
+            case 'x':
+                bit_on(FLAG_STOP_REQUEST);
+                break;
+            case 'q':
+                return 0;
+            
+            default:
+                break;
+            }
+        }
     }
-
-
 }
